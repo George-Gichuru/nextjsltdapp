3ed57135c3e3c4a388485324da774339284fef5e
GEORGE-GICHURU <gichurugeorge092@gmail.com>
2022-10-28 14:11:00 +0300

    The layout of the page and products[slug]

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React from 'react';
+import Head from 'next/head';
 
-const Layout = () => {
+import Navbar from './Navbar';
+import Footer from './Footer';
+
+const Layout = ({ children }) => {
   return (
-    <div>Layout</div>
+    <div className="layout">
+      <Head>
+        <title>The Grandson Store</title>
+      </Head>
+
+      <header>
+        <Navbar/>
+      </header>
+
+      <main className='main-container'>
+        {children}
+      </main>
+
+      <footer>
+        <Footer/>
+      </footer>
+    </div>
   )
 }
 
